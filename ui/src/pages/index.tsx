0c44d7d3650349acae0ffd3054d65bc62b6c4557
Praveenan Jeevarethinam <praveenanjvp@gmail.com>
2026-05-13 09:50:04 +0530

    feat(ui): implement shipment table UI with dummy shipment data and action controls

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -2,13 +2,14 @@ import Head from "next/head";
 import { CardanoWallet, MeshBadge } from "@meshsdk/react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import ShipmentTable from "@/components/ShipmentTable";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
       <Header />
       <main className="flex-1 w-full  mx-auto px-4 sm:px-6 lg:px-8 py-8">
-       
+       <ShipmentTable/>
       </main>
       <Footer/>
     </div>
